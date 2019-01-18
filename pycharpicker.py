ffc49f4e522def7232191d7014b06c2b06bfbbd8
Paweł Sola <kudlok@mail.ru>
2019-01-18 22:13:15 +0100

    Fully functional script added to repository

---
+++ pycharpicker.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import getpass, subprocess
+pwd=getpass.getpass(prompt='Password? ')
+xdt='xdotool'
+get_win='getactivewindow'
+clearmods='--clearmodifiers'
+indexes=input("pick your chars:\n")
+output=''
+for index in indexes.split():
+    output+=pwd[int(index)-1]
+params=[
+        [get_win, 'windowclose'],
+        ['type', clearmods, '--', output],
+        [get_win, 'windowfocus'],
+        ['key', clearmods, 'Return']
+        ]
+for p in params:
+    subprocess.run([xdt] + p)
