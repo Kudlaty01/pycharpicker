493fbe9134a5938ad5db9d5369e5f56d1f001a1f
Paweł Sola <kudlok@mail.ru>
2019-01-18 22:42:34 +0100

    Improved pycharpicker.py formatting

--- pycharpicker.py
+++ pycharpicker.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import getpass, subprocess
-pwd=getpass.getpass(prompt='Password? ')
-xdt='xdotool'
-get_win='getactivewindow'
-clearmods='--clearmodifiers'
-indexes=input("pick your chars:\n")
-output=''
+pwd = getpass.getpass(prompt='Password? ')
+xdt = 'xdotool'
+get_win = 'getactivewindow'
+clearmods = '--clearmodifiers'
+indexes = input("pick your chars:\n")
+output = ''
 for index in indexes.split():
-    output+=pwd[int(index)-1]
+    output += pwd[int(index)-1]
 params=[
         [get_win, 'windowclose'],
         ['type', clearmods, '--', output],
