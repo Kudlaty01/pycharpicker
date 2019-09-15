396af1da4837f71601f975fff8bcb313c9dd9f82
Paweł Sola <kudlok@mail.ru>
2019-09-15 20:01:00 +0200

    Change char numbers input string parsing method to RegEx instead of plain spaces splitting

--- pycharpicker.py
+++ pycharpicker.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-import getpass, subprocess
+import re, getpass, subprocess
 pwd = getpass.getpass(prompt='Password? ')
 xdt = 'xdotool'
 get_win = 'getactivewindow'
 clearmods = '--clearmodifiers'
 indexes = input("pick your chars:\n")
 output = ''
-for index in indexes.split():
+for index in re.findall(r'\d+', indexes):
     output += pwd[int(index)-1]
 params=[
         [get_win, 'windowclose'],
